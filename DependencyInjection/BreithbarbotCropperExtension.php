db0cf7cf61c0bdb41057b768bb53402b3fd4e9e5
Joel <joel.abudu@devit-team.com>
2021-08-10 01:46:54 +0000

    Rearrange file

--- DependencyInjection/BreithbarbotCropperExtension.php
+++ DependencyInjection/BreithbarbotCropperExtension.php
@@ -1,7 +1,7 @@
 <?php
 namespace Breithbarbot\CropperBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 class BreithbarbotCropperExtension extends Extension
@@ -10,10 +10,7 @@ class BreithbarbotCropperExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $container->setParameter('breithbarbot_cropper.default_folder', $config['config']['default_folder']);
         $container->setParameter('breithbarbot_cropper.data_class', $config['config']['data_class']);
