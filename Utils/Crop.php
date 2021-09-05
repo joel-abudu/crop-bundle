ec618be7784180678646135b9bae86972177985d
Joel <joel.abudu@devit-team.com>
2021-09-05 20:15:46 +0000

    Fix test folder

--- Utils/Crop.php
+++ Utils/Crop.php
@@ -47,7 +47,9 @@ class Crop
                 foreach ($each_folder as $item) {
                     if (!empty($item)) {
                         $pathname .= $item."/";
-                        mkdir($pathname);
+                        if (!is_dir($pathname)) {
+                            mkdir($pathname);
+                        }
                     }
                 }
             }
