0b249c997005d44852f3de0b7e04153857f18643
Joel <joel.abudu@devit-team.com>
2021-12-31 04:34:53 +0000

    Improve example file

--- Resources/doc/exemples/Controller/UserController.php
+++ Resources/doc/exemples/Controller/UserController.php
@@ -26,13 +26,8 @@ class UserController extends Controller
             if (!empty($result_avatar_input)) {
                 try {
                     $file = new File();
-                    $file->setFullPath($result_avatar_input->getPathname());
                     $file->setPath(str_replace('\\', '/', str_replace($this->getParameter('kernel.project_dir').'/public', '', $result_avatar_input->getPathname())));
                     $file->setName($name);
-                    $file->setMimeType(mime_content_type($result_avatar_input->getPathname()));
-                    $file->setSize($result_avatar_input->getSize());
-                    $file->setTitle($avatar_input->getClientOriginalName());
-                    $file->setLegend($avatar_input->getClientOriginalName());
                     $em->persist($file);
                     $user->setAvatar($file);
                     $em->persist($user);
