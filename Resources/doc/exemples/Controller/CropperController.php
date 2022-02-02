055f61e0b61ca14e98d13f2f696445cd9ff3757f
Joel <joel.abudu@devit-team.com>
2022-02-02 21:52:26 +0000

    Update namespace

--- Resources/doc/exemples/Controller/CropperController.php
+++ Resources/doc/exemples/Controller/CropperController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller\Admin;
+namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
