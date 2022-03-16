706ad838504ee462efcb36783c2b152e0d62d23b
Joel <joel.abudu@devit-team.com>
2022-03-16 20:12:06 +0000

    chore: improve custom Twig Extension

--- Twig/Extension/ParameterExtension.php
+++ Twig/Extension/ParameterExtension.php
@@ -1,29 +1,29 @@
 <?php
 namespace Breithbarbot\CropperBundle\Twig\Extension;
 use Symfony\Component\DependencyInjection\Container;
-use Twig_Extension;
-use Twig_SimpleFunction;
-class ParameterExtension extends Twig_Extension
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+class ParameterExtension extends AbstractExtension
 {
     private Container $container;
     public function __construct(Container $container)
     {
         $this->container = $container;
     }
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
-            new Twig_SimpleFunction('breithbarbot_cropper_parameter', [$this, 'getParameter']),
+            new TwigFunction('breithbarbot_cropper_parameter', [$this, 'getParameter']),
         ];
     }
-    public function getParameter($name)
+    public function getParameter(string $name): string
     {
         if (!empty($name)) {
             return $this->container->getParameter($name);
         }
         return '';
     }
-    public function getName()
+    public function getName(): string
     {
         return 'breithbarbot_cropper_parameter';
     }
